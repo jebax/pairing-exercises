3d607021c4c2ff8032893caacdfe663588863497
jebax <joel.eliot.baxter@gmail.com>
2018-10-01 16:25:46 +0100

    passed test for 'street'

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,7 +1,11 @@
 class Scrabble
   SCORES = {
-    a => 1,
-    f => 4
+    "a" => 1,
+    "e" => 1,
+    "f" => 4,
+    "r" => 1,
+    "s" => 1,
+    "t" => 1
   }
   def initialize(word)
     @word = word
@@ -10,5 +14,7 @@ class Scrabble
   def score
     return 0 if @word == nil
     return 0 unless @word.match(/[A-Za-z]/)
+    letters = @word.chars
+    letters.map{|letter| SCORES[letter]}.sum
   end
 end
