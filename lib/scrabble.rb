810eeec9254b157419e6e0716d5a332498352853
jebax <joel.eliot.baxter@gmail.com>
2018-10-01 16:32:12 +0100

    passed all basic tests

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,16 +1,31 @@
 class Scrabble
   SCORES = {
     "a" => 1,
+    "b" => 3,
+    "c" => 4,
+    "d" => 2,
     "e" => 1,
     "f" => 4,
+    "g" => 2,
+    "h" => 4,
     "i" => 1,
+    "j" => 8,
     "k" => 5,
+    "l" => 1,
+    "m" => 3,
+    "n" => 1,
+    "o" => 1,
+    "p" => 3,
     "q" => 10,
     "r" => 1,
     "s" => 1,
     "t" => 1,
     "u" => 1,
-    "y" => 4
+    "v" => 4,
+    "w" => 4,
+    "x" => 8,
+    "y" => 4,
+    "z" => 10
   }
   def initialize(word)
     @word = word
@@ -19,7 +34,7 @@ class Scrabble
   def score
     return 0 if @word == nil
     return 0 unless @word.match(/[A-Za-z]/)
-    letters = @word.chars
+    letters = @word.downcase.chars
     letters.map{|letter| SCORES[letter]}.sum
   end
 end
