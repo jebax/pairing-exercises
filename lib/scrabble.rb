b1b6397df862a54632ec4391e25e523a724924a7
jebax <joel.eliot.baxter@gmail.com>
2018-10-01 16:28:19 +0100

    passed test for 'quirky'

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -3,9 +3,14 @@ class Scrabble
     "a" => 1,
     "e" => 1,
     "f" => 4,
+    "i" => 1,
+    "k" => 5,
+    "q" => 10,
     "r" => 1,
     "s" => 1,
-    "t" => 1
+    "t" => 1,
+    "u" => 1,
+    "y" => 4
   }
   def initialize(word)
     @word = word
